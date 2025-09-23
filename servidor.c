ce48eb3b64a0e0926abf26fc4282a7a3c737ae64
rauulfd <raul.de.la@rai.usc.es>
2025-09-23 10:58:40 +0200

    a

--- servidor.c
+++ servidor.c
@@ -1,4 +1,5 @@
 #include <sys/types.h>
+#include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
@@ -11,17 +12,45 @@ int main()
 
     int idSocketS;
     int idSocketC;
+    int sockcon;
     struct sockaddr_in ipportserv;
+    struct sockaddr_in ipportcli;
+    socklen_t tamano = sizeof(struct sockaddr_in);
+    char mensaje[] = "Raul maricon";
 
-    if (idSocketS = socket(AF_INET, SOCK_STREAM, 0) < 0)
+    memset(&ipportserv, 0, sizeof(ipportserv));
+    ipportserv.sin_family = AF_INET;
+    ipportserv.sin_addr.s_addr = htonl(INADDR_ANY); // aceptar peticiones de cualquier interfaz
+    ipportserv.sin_port = htons(8080);              // puerto en orden de red
+
+    idSocketS = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (idSocketS < 0)
     {
         perror("No se pudo crear el socket ");
         exit(EXIT_FAILURE);
     }
 
-    ipportserv.sin_family = AF_INET;
-    ipportserv.sin_addr.s_addr = htonl(INADDR_ANY);
-    ipportserv.sin_port = htons(8080);
+    if (bind(idSocketS, (struct sockaddr *)&ipportserv, sizeof(struct sockaddr_in)) < 0)
+    {
+        perror("No se pudo asignar direccion ");
+        exit(EXIT_FAILURE);
+    }
 
-    bind(idSocketS, (struct sockaddr_in))
+    listen(idSocketS, 10);
+
+    if ((sockcon = accept(idSocketS, (struct sockaddr *)&ipportcli, &tamano)) < 0)
+    {
+        perror("No se pudo aceptar la conexion");
+        exit(EXIT_FAILURE);
+    }
+    printf("Se ha conectado un cliente con ip %s al puerto %d\n", inet_ntoa(ipportcli.sin_addr), ntohs(ipportcli.sin_port));
+
+    ssize_t bytes = send(sockcon, mensaje, sizeof(mensaje), 0);
+    printf("Se han enviado %zd bytes\n", bytes);
+
+    close(sockcon);
+    close(idSocketS);
+
+    return 0;
 }
